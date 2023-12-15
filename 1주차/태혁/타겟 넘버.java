f7b1c27b80ffda5d0e42d81d8fdf2ef1e3cf31d8
rlaxoqkf <50920256+rlaxoqkf@users.noreply.github.com>
2023-12-15 16:27:04 +0900

    Update 타겟 넘버.java

--- 1주차/태혁/타겟 넘버.java
+++ 1주차/태혁/타겟 넘버.java
@@ -1,1 +1,36 @@
-
+import java.util.*;
+class Solution {
+    public void makemask(ArrayList<int[]> mask, int[] currentMask, int n, int index){
+        if(index == n){
+            int[] newMask = currentMask.clone();
+            mask.add(newMask);
+            return;
+        }
+        currentMask[index] = 1;
+        makemask(mask, currentMask, n, index + 1);
+        
+        currentMask[index] = -1;
+        makemask(mask, currentMask, n, index + 1);
+    }
+    
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        ArrayList<int[]> mask = new ArrayList<>();
+        int n = numbers.length;
+        int[] currentMask = new int[n];
+        
+        makemask(mask, currentMask, n, 0);
+        
+        for(int[] arr : mask){
+            int sum = 0;
+            int i = 0;
+            for(int k : arr){
+                sum += k * numbers[i];
+                i++;
+            }
+            if(sum==target)
+                answer++;
+        }
+        return answer;
+    }
+}
