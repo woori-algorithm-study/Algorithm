da059ed1a0e7b9a22f7982cbd0c479bbf0a74014
PARK SEONJU <101613808+mimimya@users.noreply.github.com>
2023-12-18 19:25:13 +0900

    Update 문자열 밀기.java

--- 1주차/선주/문자열 밀기.java
+++ 1주차/선주/문자열 밀기.java
@@ -2,16 +2,15 @@ class Solution {
     public int solution(String A, String B) {
         int toRightCnt = 0;
         boolean there = false;
-        String A_copy = A;
         
         for(int i=0; i<A.length();i++){
             
-            if(A_copy.equals(B)) {there=true; break;}
+            if(A.equals(B)) {there=true; break;}
             
-            char last = A_copy.charAt(A.length()-1);
-            String front = A_copy.substring(0, A.length()-1);
+            char last = A.charAt(A.length()-1);
+            String front = A.substring(0, A.length()-1);
 
-            A_copy = last + front;
+            A = last + front;
             toRightCnt++;
         }
         
