75b932fa68349ebb99260fa71176848373769261
PARK SEONJU <101613808+mimimya@users.noreply.github.com>
2023-12-18 12:51:42 +0900

    Update 치킨쿠폰.py

--- 1주차/선주/치킨쿠폰.py
+++ 1주차/선주/치킨쿠폰.py
@@ -1,9 +1,10 @@
 def solution(chicken):
     answer = -1
-    coupone = chicken/10
-    service = 0
-    while (coupone >0):
-        service += coupone
-        coupone //=10   
+    coupone = chicken%10 + chicken//10
+    service = chicken//10
     
-    return service # 실행한 결괏값 119.1이 기댓값 120과 다릅니다.
+    while(coupone > 9):
+        service += coupone//10
+        coupone = coupone%10 + coupone//10
+        
+    return service
