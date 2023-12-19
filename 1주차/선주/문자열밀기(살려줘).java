0350cb41827e3406dbe100801a79af4fea1f40e3
PARK SEONJU <101613808+mimimya@users.noreply.github.com>
2023-12-19 14:01:37 +0900

    문자열밀기 오류 원인파악완료

--- 1주차/선주/문자열밀기(살려줘).java
+++ 1주차/선주/문자열밀기(살려줘).java
@@ -5,25 +5,17 @@ class Solution {
         int toLeft =0;
         boolean there = false;
         String A_copy = A;
-        //if len>1 for 스킵하면 A가 B와 같은지 판단 못함
+	    
         for(int i=0; i<A.length();i++){
             if(A_copy.equals(B)) {there=true; break;}
             char last = A_copy.charAt(A.length()-1);
-            String front = A_copy.substring(0, A.length()-2);
+            String front = A_copy.substring(0, A.length()-2);//Error!
             A_copy = last + front;
             System.out.println(A_copy);
             toRight++;
         }
-
-        for(int j=A.length()-1; j>=0; j--){
-            if(A_copy.equals(B)) {there=true; break;}
-            char first = A_copy.charAt(0);
-            String back = A_copy.substring(1, A.length()-1);
-            A_copy = back + first;
-            toLeft++;
-        }
         
-        return there? ( (toRight> toLeft)? toLeft : toRight ): -1;
+        return there? toRight: -1;
     }
 }
 
@@ -35,4 +27,11 @@ Exception in thread "main" java.lang.StringIndexOutOfBoundsException: String ind
 	at SolutionTest.lambda$main$0(Unknown Source)
 	at SolutionTest$SolutionRunner.run(Unknown Source)
 	at SolutionTest.main(Unknown Source)
+
+ 
+ 오류 원인 :  A_copy.substring(0, A.length()-2);
+String	substring(int beginIndex) : Returns a string that is a substring of this string.
+String	substring(int beginIndex, int endIndex) : Returns a string that is a substring of this string.
+
+The endIndex includes itself in the returned interval.
 */
