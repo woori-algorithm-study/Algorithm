fd3a31934065b85d57368a31f0fa23a295653afa
rlaxoqkf <50920256+rlaxoqkf@users.noreply.github.com>
2023-12-19 10:04:05 +0900

    Update 문자열 밀기.java

--- 1주차/태혁/문자열 밀기.java
+++ 1주차/태혁/문자열 밀기.java
@@ -1,1 +1,32 @@
-
+import java.util.*;
+class Solution {
+    public int solution(String A, String B) {
+        int answer = -1;
+        ArrayList<Integer> answers = new ArrayList<>();
+        if(A.equals(B))
+            answer = 0;
+        else{
+            for(int i = 0; i < A.length()-1; i++){
+                String C = "";
+                C += A.charAt(A.length()-1);
+                System.out.println("A: " + A);
+                for(int j = 0; j < A.length()-1; j++){
+                    C += A.charAt(j);
+                }
+                if(C.equals(B)) answers.add(i+1);
+                A = C;
+            }
+            int min = 101;
+            for(Integer a : answers){
+                if(a < min) min = a;
+            }
+        
+            if(answers.size()==0)
+                answer = -1;
+            else
+                answer = min;
+        }
+        
+        return answer;
+    }
+}
