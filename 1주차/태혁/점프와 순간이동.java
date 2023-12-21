4b78a9c662a1cec12ae7a25fd3fac6397673082b
rlaxoqkf <50920256+rlaxoqkf@users.noreply.github.com>
2023-12-21 09:33:01 +0900

    Update 점프와 순간이동.java

--- 1주차/태혁/점프와 순간이동.java
+++ 1주차/태혁/점프와 순간이동.java
@@ -1,1 +1,18 @@
 
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        
+        while(n>0){
+            if(n%2 == 0){
+                n /= 2;
+            }else{
+                n = n-1;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
