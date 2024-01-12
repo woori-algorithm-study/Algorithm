9d21da03323d5c8dbc5267d2898749105dfb13ce
PARK SEONJU <101613808+mimimya@users.noreply.github.com>
2024-01-12 12:35:32 +0900

    석유시추.java 수도코드

---
+++ 4주차/선주/석유시추.java
@@ -0,0 +1,17 @@
+class Solution {
+    int[] go = {1, -1}
+    public int solution(int[][] land) {
+        int answer = 0;
+        //각 세로줄을 실제로 시추해보기
+        for(int i = 0; i<m; i++){
+            //계속 아래로 가다가
+            //석유를 만나면 // 석유 +1
+            // (반복) 사방의 위치로 갔을 때 더이상 갈 위치가 없으면
+                //방문 하지 않았고, 석유가 있으면  //석유+1
+                //현재 위치를 갱신
+                //더이상 갈 위치가 없으면 break;
+            
+        }
+        return answer;
+    }
+}
