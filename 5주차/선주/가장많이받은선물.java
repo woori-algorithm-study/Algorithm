763c3d68d9665749cba138109a03e6cd999c8894
PARK SEONJU <101613808+mimimya@users.noreply.github.com>
2024-01-15 18:37:57 +0900

    슈도코드

---
+++ 5주차/선주/가장많이받은선물.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        int answer = 0;
+        //friends 읽으면서
+        //hashmap : 이름 , 선물지수 :0 초기화
+            //gifts 읽으면서
+            //friends 자신과 같으면 자신의 선물지수 추가
+            //공백으로 split 해서 나온 상대방 알아내기
+            //상대방의 선물지수 깎기
+        return answer;
+    }
+}
