9042f8c42dbcadd72c13fc772c432419f32ca1d8
zzzdks760 <zzzdks760@naver.com>
2024-02-10 22:49:35 +0900

    BOJ1012

---
+++ week8/sungmin/BOJ1012.java
@@ -0,0 +1,77 @@
+// 	15888KB	168ms
+
+package week8.sungmin;
+
+import java.io.*;
+import java.util.*;
+public class BOJ1012 {
+    static int[][] map;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int T, M, N, K, answer;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            map = new int[M][N];
+            answer = 0;
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                map[x][y] = 1;
+            }
+            // System.out.println(Arrays.deepToString(map));
+            
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (map[i][j] == 1) {
+                        map[i][j] = 0;
+                        bfs(i, j);
+                        answer++;
+                    }
+                }
+            }
+            System.out.println(answer);
+        }
+    }
+
+    public static void bfs(int v, int m) {
+        Deque<Node> q = new ArrayDeque<>();
+        q.offerFirst(new Node(v, m));
+
+        while (!q.isEmpty()) {
+            Node node = q.pollFirst();
+            int x = node.x;
+            int y = node.y;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < M && ny < N) {
+                    if (map[nx][ny] == 1) {
+                        map[nx][ny] = 0;
+                        q.offerFirst(new Node(nx, ny));
+                    }
+                }
+            }
+        }
+    }
+
+    public static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
