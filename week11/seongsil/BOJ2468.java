823880f487a617e9bf7b4de777d55156e70e08b8
limfruit <148676716+limfruit@users.noreply.github.com>
2024-03-05 20:58:31 +0900

    Update BOJ2468.java

--- week11/seongsil/BOJ2468.java
+++ week11/seongsil/BOJ2468.java
@@ -11,6 +11,7 @@ public class Main {
 	static int N;
 	static int[][] map;
 	static int[][] rain; // 비온뒤
+	static Deque<int[]> dq;
 	static boolean[] height = new boolean[101];
 	
 	static int[] dx = {-1, 1, 0, 0};
@@ -25,6 +26,7 @@ public class Main {
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
+		dq = new ArrayDeque<>();
 		
 		for (int i = 0; i < N; i++) {
 			String str[] = br.readLine().split(" ");
@@ -33,55 +35,57 @@ public class Main {
 				height[map[i][j]] = true;
 			}
 		}
-		
+	
 		height[0] = true; // 아예 안 잠기는 경우도 고려해야함
 		
-		bfs();
-		System.out.println(answer);
-		
-	}
-	
-	static void bfs() {
 		for (int i = 0; i < 101; i++) {
 			if (height[i]) {
 				check(i); // 잠길 수 있는 부분 확인해서 rain 배열 만들기
+				//System.out.println(Arrays.deepToString(rain));
 				count = 0;
 				
 				for (int j = 0; j < N; j++ ) {
 					for (int k = 0; k < N; k++) {
 					
 						if (rain[j][k] == 1) {
-							Deque<int[]> dq = new ArrayDeque<int[]>();
 							dq.addLast(new int[] {j, k});
 							rain[j][k] = 0;
-						
-							while (!dq.isEmpty()) {
-								int[] xy = dq.pollFirst();
-								int x = xy[0];
-								int y = xy[1];
-							
-								for (int a = 0; a < 4; a++) {
-									int nx = x + dx[a];
-									int ny = y + dy[a];
-								
-									if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-										if (rain[nx][ny] == 1) {
-											dq.addLast(new int[] {nx, ny});
-											rain[nx][ny] = 0;
-										}
-									}	
-								}
-							}
-
+							bfs();
 							count++;
-						}		
-					}	
+						}
+					}
 				}
+				
 				answer = Math.max(answer, count);
 			}
 		}
+		
+		
+		System.out.println(answer);
+		
 	}
-    
+	
+	static void bfs() {
+		while (!dq.isEmpty()) {
+			int[] xy = dq.pollFirst();
+			int x = xy[0];
+			int y = xy[1];
+							
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+								
+				if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+					if (rain[nx][ny] == 1) {
+						dq.addLast(new int[] {nx, ny});
+						rain[nx][ny] = 0;
+					}
+				}	
+			}
+			
+		}
+	}
+	
 	static void check(int x) {
 		// x 이하인 부분은 물에 잠긴다
 		
@@ -94,4 +98,5 @@ public class Main {
 			}
 		}
 	}
+	
 }
